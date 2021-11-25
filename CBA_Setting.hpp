ef111fe725162e67e6c55a02b9535450e9fc8b2c
Aaren882 <aaren.yang@gmail.com>
2021-11-25 20:54:43 +0800

    CBA Settings

--- CBA_Setting.hpp
+++ CBA_Setting.hpp
@@ -51,3 +51,20 @@
 	["Volume Controllers", "Cockpit Simulation Settings"],
 	[0, 50, 25, 0]
 ] call CBA_fnc_addSetting;
+
+//Sync
+//CHECKBOX
+[
+	"sync_fn","CHECKBOX",
+	["Synchronize the Internal and the External Volume"],
+	["Volume Controllers", "Sync Settings (Internal and External Volume will be the 'Same')"],
+	false,
+	true
+] call CBA_fnc_addSetting;
+//Slider
+[
+	"sync_sdr", "SLIDER",
+	["Vehicle Volume (%)","Volume of the Vehicle"],
+	["Volume Controllers", "Sync Settings (Internal and External Volume will be the 'Same')"],
+	[0, 100, 100, 0]
+] call CBA_fnc_addSetting;
